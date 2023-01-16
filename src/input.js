505b6b4564b80349f121f7d60172d0cd89405c09
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2023-01-16 13:50:22 -0300

    addTaskBtn.addEventListener
    change from addTaskBtn to addTaskForm

--- src/input.js
+++ src/input.js
@@ -1,6 +1,21 @@
 const addTaskBtn = document.querySelector('#addTaskBtn');
-
+const mainNav = document.querySelector('#mainNav');
 
 addTaskBtn.addEventListener('click', () => {
-    addTaskBtn.parentNode.removeChild(addTaskBtn);
+    const addTaskForm = document.createElement('form');
+    addTaskForm.classList.add('main-nav-addTaskForm');
+
+    addTaskForm.innerHTML = `
+    <input type="text" placeholder="Add a Task" class="main-nav-addTaskForm-input">
+
+    <section class="main-nav-addTaskForm-section">
+        <button class="main-nav-addTaskForm-section-btn save">Save</button>
+        <button class="main-nav-addTaskForm-section-btn cancel">Cancel</button>
+    </section>
+
+    `
+
+    mainNav.insertBefore(addTaskForm, addTaskBtn);
+
+    mainNav.removeChild(addTaskBtn);
 })

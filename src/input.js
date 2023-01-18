9ec76de6cf3cfcdf229880335d3bab402c55cbcc
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2023-01-18 11:34:03 -0300

    desktop design done
    just need to make the menubar to show the project list on small screens (in other words, need to finish the mobile design)

--- src/input.js
+++ src/input.js
@@ -1,5 +1,7 @@
 const addTaskBtn = document.querySelector('#addTaskBtn');
+const addProjectBtn = document.querySelector('#addProjectBtn');
 const mainNav = document.querySelector('#mainNav');
+const mainAsideLeft = document.querySelector('#main-aside-left');
 
 addTaskBtn.addEventListener('click', () => {
     const addTaskForm = document.createElement('form');
@@ -18,4 +20,23 @@ addTaskBtn.addEventListener('click', () => {
     mainNav.insertBefore(addTaskForm, addTaskBtn);
 
     mainNav.removeChild(addTaskBtn);
+})
+
+addProjectBtn.addEventListener('click', () => {
+    const addProjectForm = document.createElement('form');
+    addProjectForm.classList.add('main-aside-left-inputForm');
+
+    addProjectForm.innerHTML = `
+    <input type="text" placeholder="Add a Task" class="main-aside-left-inputForm-input">
+
+    <section class="main-aside-left-inputForm-section">
+        <button class="main-aside-left-inputForm-section-btn save">Save</button>
+        <button class="main-aside-left-inputForm-section-btn cancel">Cancel</button>
+    </section>
+
+    `
+
+    mainAsideLeft.insertBefore(addProjectForm, addProjectBtn);
+
+    mainAsideLeft.removeChild(addProjectBtn);
 })
